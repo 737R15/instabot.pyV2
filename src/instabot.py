66d3e16ae27dc29db4bbf0fd2484a3c98c55a368
Uğur Öztürk <mail@uozturk.com>
2018-01-03 19:33:26 +0300

    print same line on python2x support

--- src/instabot.py
+++ src/instabot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
 from .unfollow_protocol import unfollow_protocol
 from .userinfo import UserInfo
 import atexit
